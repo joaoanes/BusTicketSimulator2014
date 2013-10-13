a1f012659c4ecc486f8889c0e0ed820c8f05e162
Eduardo Jesus <eduardo.fernando.jesus@gmail.com>
2013-10-14 00:59:41 +0100

    datepicker doesn't show calendar. tested url connection in thread. added gitignore, ignoring bin folder.

--- Android/BusTicketPassenger/src/pt/feup/busticketpassenger/LoginActivity.java
+++ Android/BusTicketPassenger/src/pt/feup/busticketpassenger/LoginActivity.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -26,7 +26,7 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
+
 		populateCardTypeSpinner();
 	}
 
@@ -36,41 +36,65 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-	
+
 	private void populateCardTypeSpinner() {
 		Spinner spinner = (Spinner) findViewById(R.id.spinner_register_card_type);
 		// Create an ArrayAdapter using the string array and a default spinner layout
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.card_types, android.R.layout.simple_spinner_item);
+				R.array.card_types, android.R.layout.simple_spinner_item);
 		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
 		spinner.setAdapter(adapter);
 	}
-	
-	public void onLoginClick(View view) throws ClientProtocolException, IOException {
+
+	public void onLoginClick(View view) {
 		String username = ((EditText) findViewById(R.id.edittext_login_username)).getText().toString();
 		String password = ((EditText) findViewById(R.id.edittext_login_password)).getText().toString();
-		
+
 		if(username.equals("") || password.equals("")) {
 			Toast.makeText(this, "Fill All Fields", Toast.LENGTH_SHORT).show();
 			return;
 		}
-		
-		HttpClient httpclient = new DefaultHttpClient();
-	    HttpResponse response = httpclient.execute(new HttpGet("http://172.30.5.67:8080/login"));
-	    StatusLine statusLine = response.getStatusLine();
-	    if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-	        ByteArrayOutputStream out = new ByteArrayOutputStream();
-	        response.getEntity().writeTo(out);
-	        out.close();
-	        String responseString = out.toString();
-	        Toast.makeText(this, responseString, Toast.LENGTH_SHORT).show();;
-	    } else{
-	        //Closes the connection.
-	        response.getEntity().getContent().close();
-	        throw new IOException(statusLine.getReasonPhrase());
-	    }
+		Log.i("thread", "aqui");
+		(new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Log.i("thread", "aqui tambem");
+					HttpClient httpclient = new DefaultHttpClient();
+					HttpResponse response = httpclient.execute(new HttpGet("http://172.30.5.67:8080/login"));
+					StatusLine statusLine = response.getStatusLine();
+					if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+						ByteArrayOutputStream out = new ByteArrayOutputStream();
+						response.getEntity().writeTo(out);
+						out.close();
+						String responseString = out.toString();
+						Log.i("thread", "consegui");
+						Toast.makeText(getApplicationContext(), responseString, Toast.LENGTH_SHORT).show();
+					} else{
+						//Closes the connection.
+						response.getEntity().getContent().close();
+						Log.i("thread", "falhou");
+						Toast.makeText(getApplicationContext(), "falhou", Toast.LENGTH_SHORT).show();
+						throw new IOException(statusLine.getReasonPhrase());
+					}
+
+				}catch(Exception e) {
+					Log.i("thread", "falhou e lançou excepçoões");
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							Toast.makeText(getApplicationContext(), "falhou", Toast.LENGTH_SHORT).show();
+						}
+					});
+				}
+			}
+		})).start();
+
 	}
 
 }
